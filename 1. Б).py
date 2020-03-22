8a4b3513fe51d4ffe88ce7a4d1ea80f2385f3fa0
unknown <andriipostolovychus@gmail.com>
2020-03-22 12:55:05 +0200

    lab_8

--- 1. Б).py
+++ 1. Б).py
@@ -4,12 +4,23 @@
 '''
 import numpy as np
 from random import randint
-c = [[randint(-10,10), randint(-10,10), randint(-10,10)],
+import copy
+a = [[randint(-10,10), randint(-10,10), randint(-10,10)],
      [randint(-10,10), randint(-10,10), randint(-10,10)],
-     [randint(-10,10), randint(-10,10), randint(-10,10)]] #генеруємо матрицю 3*3 (як список)
-a = np.array(c) # перетворюємо список в масив
-a = a.transpose() # функцією transpose() транспонуємо матрицю
-for i in range(len(a)):
-    for j in range(len(a)):
-      print(a[i][j], end=' ') #по елемнтено виводим матрицю
+     [randint(-10,10), randint(-10,10), randint(-10,10)]]
+        #генеруємо матрицю 3*3 (як список)
+c = np.array(a) # перетворюємо список в масив
+c_copy = copy.deepcopy(c) #створюємо глибоку копію матриці
+for i in range(len(c)):
+    for j in range(len(c)):
+        for i in range(len(c_copy)):
+            for j in range(len(c_copy)):
+                c[i][j], c[j][i] = c_copy[j][i], c_copy[i][j]
+                '''
+                замінюємо елемент матриці з індексами j та i выдповыдним елеменом з
+                копії цієї матриці
+                '''
+for i in range(len(c)):
+    for j in range(len(c)):
+        print(c[i][j],end=' ')
     print()
